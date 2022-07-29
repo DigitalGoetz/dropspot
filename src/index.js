72905ca226f50b45044be211a6ec1bcc51f87630
Michael Goetz <digital.goetz@gmail.com>
2022-07-29 13:16:33 -0400

    skeleton for uploading a file

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+const port = 3000;
+
+var fs = require('fs');
+var dir = '/home/michael/dropspot/';
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+app.use(express.static('public'))
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function (req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = dir + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
